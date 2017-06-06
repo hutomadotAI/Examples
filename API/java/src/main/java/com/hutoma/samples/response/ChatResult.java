1075de0ed6f6380ab5a0dccb976c74449f759843
maurizio cibelli <mcibelli@hotmail.com>
2017-06-06 16:58:24 +0200

    Adding examples on how to call the API

---
+++ API/java/src/main/java/com/hutoma/samples/response/ChatResult.java
@@ -0,0 +1,15 @@
+package com.hutoma.samples.response;
+
+public class ChatResult {
+
+    private String answer;
+    private String history;
+
+    public String getAnswer() {
+        return this.answer;
+    }
+
+    public String getHistory() {
+        return this.history;
+    }
+}
---
+++ API/java/src/main/java/com/hutoma/samples/response/Status.java
@@ -0,0 +1,14 @@
+package com.hutoma.samples.response;
+
+public class Status {
+    private int code;
+    private String info;
+
+    public int getCode() {
+        return this.code;
+    }
+
+    public String getInfo() {
+        return this.info;
+    }
+}
