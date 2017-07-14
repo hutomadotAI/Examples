18b0b187e6c429acbee63cd580c72cdc5b148aab
Paul Annetts <paul.annetts@hutoma.com>
2017-07-14 19:27:15 +0100

    Read variable dictionary

--- WebHooks/main.py
+++ WebHooks/main.py
@@ -2,44 +2,37 @@ import argparse
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
 
-app = Flask(__name__)
-api = Api(app)
-
-# Use RequestParser to pull out the intentName, memoryVariables and chatResult.
-parser = reqparse.RequestParser()
-parser.add_argument('intentName')
-parser.add_argument('memoryVariables', type=dict, action='append')
-parser.add_argument('chatResult', type=dict)
-
 class HelloWorld(Resource):
     # This recieves requests that use the POST method.
     def post(self):
         # Uses the request parser to extract what we want from the JSON.
-        args = parser.parse_args()
+        args = REQUEST_PARSER.parse_args()
         intent = str(args['intentName'])
-        variables = args['memoryVariables']
+        variables = args['variablesMap']
 
         # Captures the first variable and it's currentValue.
-        colourVariable = variables[0]
-        colour = colourVariable['currentValue']
+        colour_variable = variables['colour']
+        colour = colour_variable['value']
+
+        print("colour is " + colour)
 
         # A default answer if we don't have an option for that value.
-        result = "I don't recognise that colour."
 
         # Set the response text based on the colour currentValue.
-        if colour is "red":
+        if colour == "red":
             result = "Red is the same colour as a Strawberry."
-        elif colour is "green":
+        elif colour == "green":
             result = "Green is the colour of the grass."
-        elif colour is "blue":
+        elif colour == "blue":
             result = "Blue is the colour of the sky."
-        elif result is "yellow":
+        elif colour == "yellow":
             result = "Yellow is the colour of the sun."
+        else:
+            result = "I don't recognise that colour."
 
         # The response in the form {"text": "_ is the colour of _."} and an OK.
         return {"text": result}, 200
 
-api.add_resource(HelloWorld, '/')
 
 if __name__ == "__main__":
     PARSER = argparse.ArgumentParser(description='Example bot launcher')
@@ -47,4 +40,15 @@ if __name__ == "__main__":
                         default=5000)
     
     BUILD_ARGS = PARSER.parse_args()
-    app.run(host='0.0.0.0', debug=True, port=BUILD_ARGS.port)
+
+    APP = Flask(__name__)
+    API = Api(APP)
+
+    # Use RequestParser to pull out the intentName, memoryVariables and chatResult.
+    REQUEST_PARSER = reqparse.RequestParser()
+    REQUEST_PARSER.add_argument('intentName')
+    REQUEST_PARSER.add_argument('variablesMap', type=dict)
+    REQUEST_PARSER.add_argument('chatResult', type=dict)
+    API.add_resource(HelloWorld, '/')
+    
+    APP.run(host='0.0.0.0', debug=True, port=BUILD_ARGS.port)
